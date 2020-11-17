4a9c57137096d45dbc4d893213578d6dceb1d00b
Christopher Blount <christopher.blount@outlook.com>
2020-11-17 14:43:28 -0600

    initial commit

---
+++ demos/formtosheets/index.js
@@ -0,0 +1,5 @@
+function thankYou(){ 
+    let firstName = document.getElementById("firstName").value;
+    let confirmation = "Thank you for filling out this form, " + firstName + "!";
+    alert(confirmation);
+}
---
+++ js/index.js
@@ -0,0 +1,11 @@
+function about() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("specialties").style.display = "none";
+}
+
+function specialties() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("specialties").style.display = "block";
+}
